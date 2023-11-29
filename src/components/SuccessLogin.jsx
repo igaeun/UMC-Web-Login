c85d8d74088bf5b7f15bf3fd242d0de21152b73c
igaeun <igaeun0112@gmail.com>
2023-11-29 12:07:08 +0900

    로그인 완료 화면 수정

--- src/components/SuccessLogin.jsx
+++ src/components/SuccessLogin.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { clearUser } from '../redux/reducer/userSlice'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Button = styled.button`
+  padding: 4px;
+  border-radius: 10px;
+  border-width: 2px;
+  background-color: white;
+  color: black;
+  width: 175px;
+  font-weight: 700;
+`
 
 function SuccessLogin () {
   const user = useSelector((state) => state.user)
@@ -10,11 +27,11 @@ function SuccessLogin () {
     dispatch(clearUser(user))
   }
   return (
-        <>
-            <h1>MyPage</h1>
-            <p>{`${user.name}(${user.id})`}님, 안녕하세요!</p>
-            <button onClick={() => LogoutFunc()}>로그아웃</button>
-        </>
+        <Container>
+            <h1>로그인</h1>
+            <p>{`${user.id}`}님, 안녕하세요.</p>
+            <Button onClick={() => LogoutFunc()}>로그아웃</Button>
+        </Container>
   )
 }
 
